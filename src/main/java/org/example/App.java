d5583f85af78d2a246d164b978d31a4e1310bedb
skillfuldev <chernonog.evgeniy@gmail.com>
2023-03-29 16:33:12 +0300

    add_configCFG

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,36 @@
 package org.example;
 
 
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ){
+public class App {
+    public static void main(String[] args) {
         // Для начала мы должны подключить наш файл Hibernate.properties, для этого используем
-        // класс Configuration
-Configuration configuration=
+        // класс Configuration, он автоматически подключается к нашему файлу
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        //Создаем сессию для работы с Hibernate
+        Session session = sessionFactory.getCurrentSession();
+        try {
+//        Начинаем транзакцию
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+
     }
 }
